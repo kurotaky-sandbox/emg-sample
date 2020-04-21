947ce39b4ba431768d5c6eef9e49b4dc41e334a0
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2020-04-21 20:46:17 +0900

    EMGの信号を読み取って、出力まで

---
+++ emg_sample/emg_sample.ino
@@ -0,0 +1,21 @@
+#include <MsTimer2.h>
+
+void emgRead() {
+  interrupts();
+  long time = micros();
+  int value = analogRead(0);
+  Serial.print(time);
+  Serial.print(" ");
+  Serial.print(value);
+  Serial.print("\n");
+  Serial.read();
+}
+
+void setup() {
+  Serial.begin(9600);
+  MsTimer2::set(100, emgRead);
+  MsTimer2::start();
+}
+
+void loop() {
+}
---
+++ emg_serial.py
@@ -0,0 +1,40 @@
+import serial
+import numpy as np
+from matplotlib import pyplot as plt
+
+ser = serial.Serial('/dev/tty.usbmodem14201', 9600, timeout=None)
+
+
+t = np.zeros(100)
+y = np.zeros(100)
+
+plt.ion()
+plt.figure()
+li, = plt.plot(t, y)
+plt.ylim(0, 5)
+plt.xlabel("time[s]")
+plt.ylabel("Voltage[V]")
+
+ser.write("*".encode())
+data = ser.readline().strip().rsplit()
+tInt = float(data[0])
+
+while True:
+    try:
+        #line = ser.readline()
+        #print(line)
+        ser.write("*".encode())
+        data = ser.readline().strip().rsplit()
+        t = np.append(t, (float(data[0])-tInt)/10**6)
+        t = np.delete(t, 0)
+        y = np.append(y, float(data[1])*5/1023)
+        y = np.delete(y, 0)
+
+        li.set_xdata(t)
+        li.set_ydata(y)
+        plt.xlim(min(t), max(t))
+        # plt.draw()
+        plt.pause(.01)
+    except KeyboardInterrupt:
+        ser.close()
+        break
